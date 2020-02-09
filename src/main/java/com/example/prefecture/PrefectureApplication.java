c779e3634cf35beb84b2b1515b8fdd35289b2b27
ecormaksin <turuth_jp_4133@yahoo.co.jp>
2020-02-09 11:37:11 +0900

    first commit

---
+++ src/main/java/com/example/prefecture/PrefectureApplication.java
@@ -0,0 +1,13 @@
+package com.example.prefecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrefectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrefectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/prefecture/PrefectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.prefecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrefectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
